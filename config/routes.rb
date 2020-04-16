cc16db58d97666696321fd00a2cb34899a45b35e
mmolloy88 <mmolloy88@outlook.com>
2020-04-16 15:13:02 +1000

    annotating entries for reference

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  #show all entries in the index
   get "/celebrities", to: "celebrities#index"
+  #just show certain aspects -- in this case a list of names
   get "/celebrities/:name", to: "celebrities#show"
+  #add additional content
   post "/celebrities", to: "celebrities#create"
+  #change or patch certain parts of site
   put "/celebrities/:name", to: "celebrities#update"
+  # remove from app
   delete "/celebrities/:name", to: "celebrities#destroy"
 end
